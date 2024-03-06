a5d3b1ffc4588517eba1c8b6221121b424a12bd8
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-06 20:05:11 -0300

    fix

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const config = require('./config/config');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL || "postgres://default:92BpGqMdXVrv@ep-white-butterfly-a435d8j5-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require", {
 
   dialect: dbConfig.dialect,
   dialectOptions: dbConfig.dialectOptions,
