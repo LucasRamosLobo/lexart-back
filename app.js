8662e55af77eab850ad4c3bc6620859ab32f045d
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-06 20:22:04 -0300

    use cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const sequelize = require("./db");
 const userRoutes = require("./router/router");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Rotas
