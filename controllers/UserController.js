70de27e42a22cabc41fb36ff90dede870cc459b7
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-06 22:36:03 -0300

    fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,27 +1,16 @@
 // controllers/UserController.js
 const userService = require('../services/userService');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
-
 
 async function registerUser(req, res) {
   try {
     const { username, password } = req.body;
-
-    // Certifique-se de que a função `registerUser` do serviço está funcionando corretamente
     await userService.registerUser(username, password);
-
-    // Consulta novamente o usuário recém-criado
-    const createdUser = await User.findOne({ where: { username } });
-
-    if (!createdUser) {
-      throw new Error('Erro ao criar o usuário');
-    }
-
-    res.status(201).json({ message: 'Usuário registrado com sucesso!', user: createdUser });
+    res.status(201).json({ message: 'Usuário registrado com sucesso!' });
   } catch (error) {
     console.error('Erro no registro:', error);
     res.status(500).json({ error: 'Erro interno no servidor', details: error.message });
+
   }
 }
 
