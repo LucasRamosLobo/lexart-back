c7e58fd7d937e6678f391dddadd10c3e4f90d739
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-06 22:16:57 -0300

    fix

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize({
   username: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
-  port: process.env.POSTGRES_PORT || 7000,
+  port: process.env.POSTGRES_PORT || 5432,
   dialect: dbConfig.dialect,
   dialectModule: pg,
   dialectOptions: dbConfig.dialectOptions,
