cfc4b6ebec2af92a29161644ceb68a11304f30b9
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-07 17:37:59 -0300

    feat: ajusta estrutura 3

--- services/productService.js
+++ services/productService.js
@@ -34,23 +34,23 @@ async function createProductFromDetails(name, details, price) {
 }
 
 async function createProductsFromArray(productsArray) {
-  const createdProducts = [];
+  const createdProducts = await Promise.all(
+    productsArray.map(async (productData) => {
+      const { name, brand, model, data } = productData;
+      const createdProduct = await createProduct(name, brand, model, null, null);
 
-  for (const productData of productsArray) {
-    const { name, brand, model, data } = productData;
-    const createdProduct = await createProduct(name, brand, model, null, null);
+      if (data && Array.isArray(data)) {
+        await Promise.all(
+          data.map(async (dataItem) => {
+            const { price, color } = dataItem;
+            await createProductData(createdProduct.id, name, { price, color });
+          })
+        );
+      }
 
-    if (data && Array.isArray(data)) {
-      await Promise.all(
-        data.map(async (dataItem) => {
-          const { price, color } = dataItem;
-          await createProductData(createdProduct.id, name, { price, color });
-        })
-      );
-    }
-
-    createdProducts.push(createdProduct);
-  }
+      return Product.findByPk(createdProduct.id);
+    })
+  );
 
   return createdProducts;
 }
