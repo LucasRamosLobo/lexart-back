813cdeb8a615712e3ac3e6abbf5c8253b62a2f82
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-06 21:40:33 -0300

    fix

--- db.js
+++ db.js
@@ -1,17 +1,26 @@
+import { Sequelize } from 'sequelize';
 import pg from 'pg';
+import config from './config/config';
 
-const { Pool } = pg;
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: dbConfig.dialect,
+  dialectModule: pg, // Indica que estamos utilizando o pacote pg para a conexão
+  dialectOptions: dbConfig.dialectOptions,
+  define: {
+    timestamps: false, // Desativar a criação automática de campos de timestamp
+  },
 });
 
-pool.on('connect', () => {
-  console.log('Conexão com o banco de dados estabelecida com sucesso.');
-});
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+})();
 
-pool.on('error', (err) => {
-  console.error('Erro na conexão com o banco de dados:', err);
-});
-
-module.exports = pool;
+module.exports = sequelize;
