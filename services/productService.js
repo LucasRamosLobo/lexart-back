3c691a6022fa56a5eb1baed1205ab5e198fb32af
LucasRamosLobo <lucasramoslobo83@gmail.com>
2024-03-07 08:39:48 -0300

    feat: aceita os 3 tipos de body

--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,4 @@
-// services/productService.js
-const { Product } = require('../models/Product');
+const { Product } = require('../models');
 
 async function getAllProducts() {
   return Product.findAll();
@@ -31,6 +30,7 @@ async function deleteProduct(id) {
   }
 
   await product.destroy();
+  return { message: 'Produto apagado com sucesso', status: 200 };
 }
 
 module.exports = {
